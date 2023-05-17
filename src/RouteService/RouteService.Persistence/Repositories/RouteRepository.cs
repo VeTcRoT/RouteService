11d4030733afc4f4c98fe7aafa831c51ab7f26c5
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-05-17 15:06:59 +0300

    Some refactoring to Route Repository

--- src/RouteService/RouteService.Persistence/Repositories/RouteRepository.cs
+++ src/RouteService/RouteService.Persistence/Repositories/RouteRepository.cs
@@ -15,40 +15,33 @@ namespace RouteService.Persistence.Repositories
 
         public async Task<IEnumerable<Route>?> GetAvailableRoutesByQueryAsync(string from, string to, DateTime departureTime, int numberOfSeats)
         {
-            List<Route> routes = await _dbContext.Routes
-                .Where(r => r.From == from && r.To == to && r.DepartureTime == departureTime && r.SeatsAvailable >= numberOfSeats).ToListAsync();
+            List<Route> routes = new ();
 
-            if (routes.Count == 0)
+            var commonRoutes = await _dbContext.Routes
+                .Where(r => r.From == from && r.DepartureTime == departureTime && r.SeatsAvailable >= numberOfSeats)
+                .Join(
+                    _dbContext.Routes.Where(r => r.To == to && r.SeatsAvailable >= numberOfSeats),
+                    routeFrom => routeFrom.RouteId,
+                    routeTo => routeTo.RouteId,
+                    (routeFrom, routeTo) => new { RouteFrom = routeFrom, RouteTo = routeTo }
+                )
+                .ToListAsync();
+
+            foreach (var route in commonRoutes)
             {
-                routes = new List<Route>();
-
-                var commonRoutes = await _dbContext.Routes
-                    .Where(r => r.From == from && r.DepartureTime == departureTime && r.SeatsAvailable >= numberOfSeats)
-                    .Join(
-                        _dbContext.Routes.Where(r => r.To == to && r.SeatsAvailable >= numberOfSeats),
-                        routeFrom => routeFrom.RouteId,
-                        routeTo => routeTo.RouteId,
-                        (routeFrom, routeTo) => new { RouteFrom = routeFrom, RouteTo = routeTo }
-                    )
-                    .ToListAsync();
-
-                foreach (var route in commonRoutes)
+                var neededRoute = new Route()
                 {
-                    var neededRoute = new Route()
-                    {
-                        Id = route.RouteFrom.Id,
-                        RouteId = route.RouteFrom.RouteId,
-                        DepartureTime = route.RouteFrom.DepartureTime,
-                        ArrivalTime = route.RouteTo.ArrivalTime,
-                        From = route.RouteFrom.From,
-                        To = route.RouteTo.To,
-                        SeatsAvailable = route.RouteFrom.SeatsAvailable,
-                        ExtraInfo = route.RouteFrom.ExtraInfo + route.RouteTo.ExtraInfo,
-                    };
+                    Id = route.RouteFrom.Id,
+                    RouteId = route.RouteFrom.RouteId,
+                    DepartureTime = route.RouteFrom.DepartureTime,
+                    ArrivalTime = route.RouteTo.ArrivalTime,
+                    From = route.RouteFrom.From,
+                    To = route.RouteTo.To,
+                    SeatsAvailable = route.RouteFrom.SeatsAvailable,
+                    ExtraInfo = route.RouteFrom.ExtraInfo + route.RouteTo.ExtraInfo,
+                };
 
-                    routes.Add(neededRoute);
-                }
-                
+                routes.Add(neededRoute);
             }
 
             return routes;
