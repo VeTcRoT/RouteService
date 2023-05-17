fe9bc5895925b39c4af99827b2e56ebe30e96d56
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-05-17 15:56:15 +0300

    Added property NumberOfSeats to Route entity

--- src/RouteService/RouteService.Domain/Entities/Route.cs
+++ src/RouteService/RouteService.Domain/Entities/Route.cs
@@ -8,6 +8,7 @@
         public DateTime ArrivalTime { get; set; }
         public string From { get; set; } = string.Empty;
         public string To { get; set; } = string.Empty;
+        public int NumberOfSeats { get; set; }
         public int SeatsAvailable { get; set; }
         public string? ExtraInfo { get; set; }
     }
