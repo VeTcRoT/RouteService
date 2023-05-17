a68880c9088f1e98992d8b67050f69a505143548
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-05-17 14:29:37 +0300

    Implemented Unit Of Work

---
+++ src/RouteService/RouteService.Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using RouteService.Domain.Interfaces.Repositories;
+
+namespace RouteService.Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IRouteRepository RouteRepository { get; }
+        private readonly RouteServiceDbContext _dbContext;
+
+        public UnitOfWork(IRouteRepository routeRepository, RouteServiceDbContext dbContext)
+        {
+            RouteRepository = routeRepository;
+            _dbContext = dbContext;
+        }
+
+        public async Task SaveAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
