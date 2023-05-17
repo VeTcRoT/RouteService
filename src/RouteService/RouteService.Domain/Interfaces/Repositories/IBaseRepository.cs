0e3d81eb12b2c0ff18157958569ae820a669dd1e
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-05-17 11:03:44 +0300

    Added repositories interfaces

---
+++ src/RouteService/RouteService.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace RouteService.Domain.Interfaces.Repositories
+{
+    public interface IBaseRepository<T>
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> ListAllAsync();
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ src/RouteService/RouteService.Domain/Interfaces/Repositories/IRouteRepository.cs
@@ -0,0 +1,11 @@
+﻿using RouteService.Domain.Entities;
+
+namespace RouteService.Domain.Interfaces.Repositories
+{
+    public interface IRouteRepository : IBaseRepository<Route>
+    {
+        Task<IEnumerable<Route>> GetAvailableRoutesByQueryAsync(string from, string to, DateTime departureTime, int numberOfSeats);
+        Task<Route> BookRideAsync(string routeId, string from, string to, int numberOfSeats); 
+
+    }
+}
