385d9a8f6d494a0e2ed8affd49b91426161e45eb
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-05-19 13:56:44 +0300

    Added deleting RouteInfo

---
+++ src/RouteService/RouteService.Application/Features/RoutesInfo/Commands/DeleteRouteInfo/DeleteRouteInfoCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace RouteService.Application.Features.RoutesInfo.Commands.DeleteRouteInfo
+{
+    public class DeleteRouteInfoCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/RouteService/RouteService.Application/Features/RoutesInfo/Commands/DeleteRouteInfo/DeleteRouteInfoCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using RouteService.Application.Exceptions;
+using RouteService.Domain.Interfaces.Repositories;
+
+namespace RouteService.Application.Features.RoutesInfo.Commands.DeleteRouteInfo
+{
+    public class DeleteRouteInfoCommandHandler : IRequestHandler<DeleteRouteInfoCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteRouteInfoCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(DeleteRouteInfoCommand request, CancellationToken cancellationToken)
+        {
+            var routeInfo = await _unitOfWork.RouteInfoRepository.GetByIdAsync(request.Id);
+
+            if (routeInfo == null)
+                throw new NotFoundException("RouteInfo", request.Id);
+
+            _unitOfWork.RouteInfoRepository.Delete(routeInfo);
+
+            await _unitOfWork.SaveAsync();
+        }
+    }
+}
