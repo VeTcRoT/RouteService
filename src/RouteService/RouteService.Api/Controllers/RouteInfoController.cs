18e0b71532894503d1c81520982380e4ba4586e3
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-05-19 13:59:50 +0300

    Added RouteInfo controller

---
+++ src/RouteService/RouteService.Api/Controllers/RouteInfoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RouteService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RouteInfoController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
