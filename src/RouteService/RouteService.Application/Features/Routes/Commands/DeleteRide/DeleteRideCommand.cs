4e7abb7a5c06711c150e4f46cc75a286449f3951
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-05-19 11:40:44 +0300

    Added ride delete

---
+++ src/RouteService/RouteService.Application/Features/Routes/Commands/DeleteRide/DeleteRideCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace RouteService.Application.Features.Routes.Commands.DeleteRide
+{
+    public class DeleteRideCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/RouteService/RouteService.Application/Features/Routes/Commands/DeleteRide/DeleteRideCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using RouteService.Application.Exceptions;
+using RouteService.Domain.Entities;
+using RouteService.Domain.Interfaces.Repositories;
+
+namespace RouteService.Application.Features.Routes.Commands.DeleteRide
+{
+    public class DeleteRideCommandHandler : IRequestHandler<DeleteRideCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteRideCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(DeleteRideCommand request, CancellationToken cancellationToken)
+        {
+            var ride = await _unitOfWork.RouteRepository.GetByIdAsync(request.Id);
+
+            if (ride == null) 
+                throw new NotFoundException(nameof(Ride), request.Id);
+
+            _unitOfWork.RouteRepository.Delete(ride);
+
+            await _unitOfWork.SaveAsync();
+        }
+    }
+}
