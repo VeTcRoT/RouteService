a4002e05e89ecdb7d676fd626c872890bf5265ad
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-05-19 13:30:07 +0300

    Added RouteInfoRepository to UnitOfWork

--- src/RouteService/RouteService.Domain/Interfaces/Repositories/IUnitOfWork.cs
+++ src/RouteService/RouteService.Domain/Interfaces/Repositories/IUnitOfWork.cs
@@ -1,8 +1,11 @@
-﻿namespace RouteService.Domain.Interfaces.Repositories
+﻿using RouteService.Domain.Entities;
+
+namespace RouteService.Domain.Interfaces.Repositories
 {
     public interface IUnitOfWork
     {
         IRouteRepository RouteRepository { get; }
+        IBaseRepository<Route> RouteInfoRepository { get; }
         Task SaveAsync();
     }
 }
--- src/RouteService/RouteService.Persistence/Repositories/UnitOfWork.cs
+++ src/RouteService/RouteService.Persistence/Repositories/UnitOfWork.cs
@@ -1,15 +1,20 @@
-﻿using RouteService.Domain.Interfaces.Repositories;
+﻿using RouteService.Domain.Entities;
+using RouteService.Domain.Interfaces.Repositories;
 
 namespace RouteService.Persistence.Repositories
 {
     public class UnitOfWork : IUnitOfWork
     {
         public IRouteRepository RouteRepository { get; }
+
+        public IBaseRepository<Route> RouteInfoRepository { get; }
+
         private readonly RouteServiceDbContext _dbContext;
 
-        public UnitOfWork(IRouteRepository routeRepository, RouteServiceDbContext dbContext)
+        public UnitOfWork(IRouteRepository routeRepository, IBaseRepository<Route> routeInfoRepository, RouteServiceDbContext dbContext)
         {
             RouteRepository = routeRepository;
+            RouteInfoRepository = routeInfoRepository;
             _dbContext = dbContext;
         }
 
