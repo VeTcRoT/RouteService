5168addc667db9634d08d0980b6addbdfbc1221e
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-06-02 16:42:05 +0300

    Additional changes

--- src/RouteService/RouteService.Application/Features/Routes/Commands/BookRide/BookRideCommandValidator.cs
+++ src/RouteService/RouteService.Application/Features/Routes/Commands/BookRide/BookRideCommandValidator.cs
@@ -10,7 +10,7 @@ namespace RouteService.Application.Features.Routes.Commands.BookRide
                 .NotEmpty().WithMessage("{PropertyName} is required.")
                 .NotNull()
                 .MinimumLength(3).WithMessage("{PropertyName} length should be equal greater to 3.")
-                .MaximumLength(30).WithMessage("{PropertyName} length should not exceed 30 characters.");
+                .MaximumLength(64).WithMessage("{PropertyName} length should not exceed 30 characters.");
 
             RuleFor(q => q.From)
                 .NotEmpty().WithMessage("{PropertyName} is required.")
